e870577f87233f6b064b6f65dcb01fa10b118216
Piotr Chuchla <piotr.chuchla@itx.group>
2023-08-13 20:14:59 +0200

    feat: width fix hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,9 +6,9 @@ interface HeroInterface {
 export default function Hero(props: HeroInterface) {
   const { text, desc } = props;
   return (
-    <div className="h-28 w-3/5">
-      <p className="font-semibold text-4xl uppercase">{text}</p>
-      <p className="mt-4">{desc}</p>
+    <div className="h-28">
+      <p className="font-semibold text-4xl uppercase w-1/2">{text}</p>
+      <p className="mt-4 w-3/4">{desc}</p>
     </div>
   );
 }
